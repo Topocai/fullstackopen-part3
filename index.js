c669f94d84a2bd4be100064f98deb4abf7afba10
topocai <leonjoserp@gmail.com>
2024-03-29 02:14:10 -0300

    part3 to 3.14

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose')
 const cors = require('cors');
 
 app.use(express.static('dist'));
@@ -13,75 +14,67 @@ let morgan = require('morgan');
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url)
+.then(() => console.log("[MONGOOSE] Connected to MongoDB"))
+.catch(err => console.error("[MONGOOSE] Error in conection", err));
+
+
+const ContactModel = require('./mongo.js');
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    ContactModel.find({}).then(contacts => {
+      res.json(contacts);
+    });
 });
 
 app.put('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const body = req.body;
+
+    const id = req.params.id
+    const body = req.body
 
     if(body.name === undefined || body.number === undefined)
         return res.status(400).json({ error: 'Name or number is missing' });
 
-    persons = persons.map(person => person.id !== id ? person : body);
-    res.json(body);
+    ContactModel.findByIdAndUpdate(id, {...body})
+    .then(contact => res.json(contact))
+    .catch(() => res.status(404).end())
+
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    if(person) 
+    const id = req.params.id;
+
+    ContactModel.findById(id)
+    .then(contact => res.json(contact))
+    .catch(err => 
     {
-        res.json(person);
-    } 
-    else 
-    {
-        res.status(404).end();
-    }
+      console.error(err);
+      res.status(404).end();
+    })
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end();
+    const id = req.params.id
+    ContactModel.findByIdAndDelete(id)
+    .then(() => res.status(204).end())
+    .catch(() => res.status(404).end())
 });
 
 app.get('/info', (req, res) => {
     const date = new Date();
-    res.send(`<p>Phonebook has info for ${persons.length} people<br/>${date}</p>`);
+    ContactModel.find({}).then(contacts => {
+      res.send(`<p>Phonebook has info for ${contacts.length} people<br/>${date}</p>`);
+    })
+    .catch(() => res.status(404).end());
 });
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
-  const newPerson = {
-    id: Math.floor(Math.random() * 1000),
-    name: body.name,
-    number: body.number
-  }
 
   if (!body.name || !body.number) 
   {
@@ -89,14 +82,37 @@ app.post('/api/persons', (req, res) => {
       error: 'Name or number is missing' 
     });
   }
-  if(persons.find(person => person.name === newPerson.name)) 
+
+  ContactModel.find({}).then(contacts => 
   {
-    return res.status(409).json({ 
-      error: 'Name has already in the phonebook' 
+    if(contacts.find(person => person.name === body.name)) 
+    {
+      return res.status(409).json({ 
+        error: 'Name has already in the phonebook' 
+      });
+    }
+    else if (contacts.find(person => person.number === body.number)) 
+    {
+      return res.status(409).json({ 
+        error: 'Number has already in the phonebook' 
+      });
+    }
+
+    const newPerson = new ContactModel({
+      name: body.name,
+      number: body.number
     });
-  }
-  persons = persons.concat(newPerson);
-  res.status(201).json(newPerson);
+
+    newPerson.save()
+    .then(savedPerson => {
+      res.json(savedPerson);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+  });
+
 });
 const PORT = process.env.PORT || 3001;
 
--- mongo.js
+++ mongo.js
@@ -1,67 +1,18 @@
 const mongoose = require('mongoose')
 
-if(process.argv.length < 3)
-{
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://topocaidev:${password}@m0-cluster.jhkq2qf.mongodb.net/phonebook?retryWrites=true&w=majority`;
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect(url).then(() => 
-{
-    const contactScheme = new mongoose.Schema({
-        name: String,
-        number: String
-    });
-    
-    const contact = mongoose.model('Contact', contactScheme);
-    
-    if(process.argv.length === 3) 
-    {
-        contact.find({}).then(result => {
-            console.log('phonebook:');
-            result.forEach(contact => {
-                console.log(contact.name, contact.number);
-            });
-            mongoose.connection.close();
-        })
-        .catch(err => {
-            console.log(err);
-            mongoose.connection.close();
-        });
-    }
-    else if (process.argv.length === 4)
-    {
-        console.log("Please provide a number for the contact")
-        process.exit(1)
-    }
-    else 
-    {
-        const contactName = process.argv[3];
-        const contactNumber = process.argv[4];
-        
-        const newContact = new contact({
-            name: contactName,
-            number: contactNumber
-        });
-
-        newContact.save().then(() => {
-            console.log(`added ${contactName} number ${contactNumber} to phonebook`, newContact);
-            mongoose.connection.close();
-        })
-        .catch(err => {
-            console.log(err)
-            mongoose.connection.close();
-        })
-    }   
-})
-.catch(err => {
-    console.log(err)
-    process.exit(1)
+const contactScheme = new mongoose.Schema({
+    name: String,
+    number: String
 });
+
+const contact = mongoose.model('Contact', contactScheme);
+
+contactScheme.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject.__v
+    }
+});
+
+module.exports = contact;
 
